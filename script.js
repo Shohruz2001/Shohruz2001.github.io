7cb808a8c2e09a34fef5e56faa27610adda7f25e
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2023-11-26 23:39:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const yearSpan = document.querySelector('.current-year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
